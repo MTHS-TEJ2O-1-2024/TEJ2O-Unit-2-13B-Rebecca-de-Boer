b99a4712fb7c2ddaff77360cbab42dae0f63bbd4
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-29 11:22:52 -0400

    counts down from 4

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Rebecca de Boer
+ * Created on: Oct 2024
+ * This program counts down from four
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+let count = 4
+
+input.onButtonPressed(Button.A, function() {
+    while (count >= 0) {
+        basic.showNumber(count)
+        if (count - 1 == 3) {
+                neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+            }
+        if (count - 1 == 2) {
+                neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+            }
+        if (count - 1 == 2) {
+                neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+                neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+            }
+        if (count - 1 == 2) {
+                neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+            }
+        basic.pause(500)
+        count--    
+    }
+    basic.showIcon(IconNames.Happy)
+})
